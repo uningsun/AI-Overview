d82c0ade40479200a7b994d98909ab7bab32e690
uningsun <uning623@gmail.com>
2019-09-11 13:21:47 +0900

    to be updated

---
+++ Image/MFC/resource.h
@@ -0,0 +1,95 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ImageProcessingyis1.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ImageProcessingyis1TYPE     130
+#define ID_WINDOW_MANAGER               131
+#define IDD_DIALOG1                     310
+#define IDD_DIALOG2                     312
+#define IDD_DIALOG3                     314
+#define IDD_DIALOG4                     316
+#define IDD_DIALOG5                     318
+#define IDD_DIALOG6                     320
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1002
+#define ID_32771                        32771
+#define ID_DOWN_SAMPLING                32772
+#define ID_32773                        32773
+#define ID_UP_SAMPLING                  32774
+#define ID_32775                        32775
+#define ID_QUANTIZATION                 32776
+#define ID_32777                        32777
+#define ID_SUM_CONSTANT                 32778
+#define ID_32779                        32779
+#define ID_SUB_CONSTANT                 32780
+#define ID_32781                        32781
+#define ID_MUL_CONSTANT                 32782
+#define ID_32783                        32783
+#define ID_DIV_CONSTANT                 32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_AND_OPERATE                  32789
+#define ID_OR_OPERATE                   32790
+#define ID_XOR_OPERATE                  32791
+#define ID_32792                        32792
+#define ID_NEGA_TRANSFROM               32793
+#define ID_32794                        32794
+#define ID_GAMMA_CORRECTION             32795
+#define ID_32796                        32796
+#define ID_BINARIZATION                 32797
+#define ID_NEGA_TRANSFORM               32798
+#define ID_32799                        32799
+#define ID_STRESS_TRANSFORM             32800
+#define ID_32801                        32801
+#define ID_HISTOGRAM                    32802
+#define ID_HISTO_EQUAL                  32803
+#define ID_HISTO_SPEC                   32804
+#define ID_32805                        32805
+#define ID_HISTO_STRETCH                32806
+#define ID_END_IN_SEARCH                32807
+#define ID_32808                        32808
+#define ID_EMBOSSING                    32809
+#define ID_BLURR                        32810
+#define ID_GAUSSIAN_FILTER              32811
+#define ID_32812                        32812
+#define ID_SHARPENING                   32813
+#define ID_HPF_SHARP                    32814
+#define ID_LPF_SHARP                    32815
+#define ID_DIFF_OPERATOR_HOR            32816
+#define ID_32817                        32817
+#define ID_HOMOGEN_OPERATOR             32818
+#define ID_LAPLACIAN                    32819
+#define ID_NEAREST                      32820
+#define ID_BILINEAR                     32821
+#define ID_MEDIAN_SUB                   32822
+#define ID_MEAN_SUB                     32823
+#define ID_TRANSLATION                  32824
+#define ID_MIRROR_HOR                   32825
+#define ID_32826                        32826
+#define ID_MIRROR_VER                   32827
+#define ID_ROTATION                     32828
+#define ID_FRAME_SUM                    32829
+#define ID_FRAME_SUB                    32830
+#define ID_FRAME_MUL                    32831
+#define ID_FRAME_DIV                    32832
+#define ID_FRAME_AND                    32833
+#define ID_FRAME_OR                     32834
+#define ID_FRAME_COMB                   32835
+#define ID_BITPLAIN                     32836
+#define ID_BITPLANE                     32837
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        322
+#define _APS_NEXT_COMMAND_VALUE         32838
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
